40d1e2987c7448917e0dad449455e2658927dc70
Davin <davinmailsklh@gmail.com>
2023-05-29 22:22:24 +0700

    fix delete

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -7,7 +7,7 @@ router.route('/').get(ProductController.read)
 router.route('/:id').get(ProductController.readOne)
 router.route('/create').post(ProductController.create)
 router.route('/update/:id').put(ProductController.update)
-router.route('delete/:id').delete(ProductController.delete)
+router.route('/delete/:id').delete(ProductController.delete)
 router.route('/sell').post(ProductController.sell)
 
 export default router
